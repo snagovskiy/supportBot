8b18270c08002407a605bf439e6e506ba479ed4f
Alexander <snagovskiy@users.noreply.github.com>
2024-11-12 19:35:30 +0300

    dssf

---
+++ main.ts
@@ -0,0 +1,19 @@
+import { webhookCallback } from "https://deno.land/x/grammy@v1.31.2/mod.ts";
+// Вы можете изменить это, чтобы правильно импортировать свой объект `Bot`.
+import bot from "./index.ts";
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+Deno.serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
