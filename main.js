434d25bea907e6a829a9f05041b4efc5b8c7dd06
Alexander <snagovskiy@users.noreply.github.com>
2024-11-12 19:53:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const handleUpdate = webhookCallback(bot, "std/http");
 Deno.serve(async (req) => {
   if (req.method === "POST") {
     const url = new URL(req.url);
-    if (url.pathname.slice(1) === bot.token {
+    if (url.pathname.slice(1) === bot.token) {
       try {
         return await handleUpdate(req);
       } catch (err) {
