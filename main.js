2cd4d09402c249459461e9e0d4155b2241c0f86c
Alexander <snagovskiy@users.noreply.github.com>
2024-11-12 18:49:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { webhookCallback } from "https://deno.land/x/grammy@v1.31.2/mod.ts";
 // Вы можете изменить это, чтобы правильно импортировать свой объект `Bot`.
-import bot from "./index";
+import bot from "./index.js";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
